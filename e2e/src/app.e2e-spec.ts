aaf206dc222af9fa4ab109ad27c7cc24b0ae4d2d
Nicolás Alarcón <metalroses@hotmail.com>
2018-12-12 12:45:21 -0300

    Test Funcionales

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -11,4 +11,18 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('App');
   });
+
+it('should hve the titles', () => {
+  const titles = [
+    'What is Lorem Ipsum?',
+    'What is Lorem Ipsum?'
+  ];
+
+  const titLesList = page.getCardTitles();
+
+  titles.forEach((title, index) => {
+    expect(titLesList.get(index).getText()).toEqual(title);
+  });
+
+});
 });
